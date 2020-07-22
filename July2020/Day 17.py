c3c831f3c6a3c61a8a30bae41788e67b7b21c28b
b3z3rk_irl <nikhil4709@gmail.com>
2020-07-23 05:09:28 +0530

    Create Day 17.py

---
+++ July2020/Day 17.py
@@ -0,0 +1,8 @@
+#not my code
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        if k == len(nums):
+            return nums
+        count = Counter(nums)   
+        return heapq.nlargest(k, count.keys(), key=count.get) 
