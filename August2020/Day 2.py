b67f1ed92b948395218e0f907625c1555644138a
b3z3rk_irl <nikhil4709@gmail.com>
2020-08-01 15:17:58 +0530

    Create Day 2.py

---
+++ August2020/Day 2.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/explore/featured/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3409/
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper():
+            return True
+        if word.islower():
+            return True
+        if word[0].isupper() and word[1:].islower():
+            return True
+        else:
+            return False
