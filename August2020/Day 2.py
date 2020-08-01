dbf6f14aeed48482e26f8af7037da7fd48b7920c
b3z3rk_irl <nikhil4709@gmail.com>
2020-08-01 15:19:16 +0530

    Update Day 2.py

--- August2020/Day 2.py
+++ August2020/Day 2.py
@@ -9,3 +9,10 @@ class Solution:
             return True
         else:
             return False
+#not my soln-
+
+import re
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return re.fullmatch(r"[A-Z]*|.[a-z]*", word)
